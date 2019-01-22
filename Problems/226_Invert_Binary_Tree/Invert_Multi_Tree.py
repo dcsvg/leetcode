a5c861f7f2e6bf6ded9a0afcbe16ea8295853e35
hanleilei <dcsvgwudi@gmail.com>
2019-01-22 23:14:50 +0800

    第一次签入

--- Problems/226_Invert_Binary_Tree/Invert_Multi_Tree.py
+++ Problems/226_Invert_Binary_Tree/Invert_Multi_Tree.py
@@ -6,8 +6,23 @@ class TreeNode(object):
 
 class Solution(object):
     def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-if
+        for i in root.childs:
+            self.invertTree(i)
+        root.childs.reverse()
+
+if __name__ == "__main__":
+    root = TreeNode(0)
+    vsn1 = [2, 7, 12]
+    vs1 = [ TreeNode(x) for x in vsn1]
+    vsn2 = [1, 3, 11]
+    vs2 = [TreeNode(x) for x in vsn2]
+    vsn3 = [6, 9]
+    vs3 = [TreeNode(x) for x in vsn3]
+    root.childs = vs1
+    root.childs[0].childs = vs2
+    root.childs[1].childs = vs3
+
+    solution = Solution()
+    solution.invertTree(root)
+
+    print(root)
